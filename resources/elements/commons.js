524553dc7e5f5d92e099c6e281d6bf6bafc97555
HEJOK254 <kuba.d_pl@outlook.com>
2024-03-13 01:20:59 +0100

    Working on it

---
+++ resources/elements/commons.js
@@ -0,0 +1,19 @@
+function expandNav() {
+    // Get elements
+    var navbar = document.getElementById("navbar");
+    var title = document.getElementById("navbar-title");
+    
+    // Navbar
+    if (navbar.className === "topnav") {
+        navbar.className += " responsive";
+    } else {
+        navbar.className = "topnav";
+    }
+
+    // Title
+    if (title.className === "topnav") {
+        title.className += " responsive";
+    } else {
+        title.className = "topnav";
+    }
+}
